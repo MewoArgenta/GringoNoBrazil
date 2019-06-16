e91edd5de7044a86aa68154c9fd96cc919accf99
Mewo <wouter.vanmeel89@gmail.com>
2019-06-16 02:12:52 +0200

    express js added, searching solution for db.js

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GringoNoBrazil
+To understand how set up works:
+- http://expressjs.com/en/starter/installing.html
+- https://mongoosejs.com/docs/index.html
 WepApp with mongoDB in Api, Page to learn Portuguese words, and admin page to add exercises
+
+
+## Launch api
+set DEBUG=myapp:* & npm start
+
+##ToDo
+Check how Bart calls the db with require mongoose
---
+++ public/javascripts/db/dbInit.js
@@ -0,0 +1,23 @@
+//How to: https://mongoosejs.com/docs/index.html
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
+// the schema defines the properties of the model (=object)
+var themeSchema = new mongoose.Schema({
+    name: String
+});
+
+var Theme = mongoose.model('Theme', themeSchema);
+var comida = new Theme({ name: 'comida' });
+
+comida.save(function (err, comida) {
+    if (err) return console.error(err);
+});
+
+console.log('saved comida on database');
